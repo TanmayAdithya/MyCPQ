80d3569fde82bbc05b9622b3b6c7c66e3a0827fb
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-12-22 22:49:23 +0530

    feat: Add Products Page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,86 @@
 const Products = () => {
+  const products = [
+    {
+      rowid: 1,
+      id: 1,
+      name: 'Genwatt',
+      quantity: 2.0,
+      unitPrice: 79.9,
+    },
+  ];
+
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-3xl ">Product</h1>
+    <div>
+      <div className="flex flex-wrap items-center justify-between pt-4 w-full mb-10">
+        <h1 className="text-3xl font-normal font-custom">Products</h1>
+        <button className="flex items-center bg-orange-500 hover:bg-orange-600  text-white border-0 rounded-md px-4 py-2 hover:shadow-xl hover:rounded-xl transition-all duration-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="12"
+            width="12"
+            viewBox="0 0 448 512"
+            className="mr-2"
+          >
+            <path
+              fill="#ffffff"
+              d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+            />
+          </svg>
+          Add Product
+        </button>
+      </div>
+      {/* Search Bar */}
+      <div className="flex items-baseline border-2 rounded-lg border-gray-300 w-1/4 px-2 py-1 mb-12">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="14"
+          width="14"
+          viewBox="0 0 512 512"
+          className="fill-gray-500 mr-2"
+        >
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+        </svg>
+        <form>
+          <input
+            id="customer-info"
+            type="text"
+            placeholder="Search"
+            className="outline-none w-full bg-transparent"
+          />
+        </form>
+      </div>
+      {/* Products */}
+      <div className="w-full bg-orange-100 border-0 rounded-t-xl">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="text-left font-medium p-2 text-sky-900">
+                Product Name
+              </th>
+              <th className="text-left font-medium text-sky-900">
+                Product Code
+              </th>
+              <th className="text-left font-medium text-sky-900">Quantity</th>
+              <th className="text-left font-medium text-sky-900">Unit Price</th>
+            </tr>
+          </thead>
+          <tbody className="border-b-2 rounded-xl border-sky-950 w-full">
+            {products.map((product) => {
+              return (
+                <tr
+                  key={product.rowid}
+                  className="border-b-2 border-slate-200 bg-slate-50 text-slate-600 hover:bg-slate-100 transition-colors duration-300"
+                >
+                  <td className="p-2">{product.name}</td>
+                  <td>{product.id}</td>
+                  <td>{product.quantity}</td>
+                  <td>{`INR ${product.unitPrice}`}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
